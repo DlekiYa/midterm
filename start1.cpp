a5e84e79922ae4f55ffcd5033b0ed7621390d3b5
DlekiYa <dlekiya@yandex.ru>
2024-03-20 13:07:56 +0300

    aaaaa

---
+++ start1.cpp
@@ -0,0 +1,35 @@
+
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+const int maxn = 200000 + 10, mod = 1000000007, inf = 2147483647, half_inf = 2147483647 / 2;
+const long long inf_ll = 9223372036854775807, mul_ll = 1000000;
+#define ll long long
+
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
